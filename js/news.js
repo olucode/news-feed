7d091e335ff71a195f45956bb508fb6a8f25b0f4
fortec007 <olumightyfalomo@gmail.com>
2017-04-21 10:05:39 +0100

    Finished the design for News-In-One

--- js/news.js
+++ js/news.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	
+	function getNews()
+	{
+		var link = "https://newsapi.org/v1/sources?language=en?category=technology";
+
+		$.getJSON(link, function(data) {
+			console.log("JSON data has been retrieved");
+		});
+	}
+
+	getNews();
+
+
 });
