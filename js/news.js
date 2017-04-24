ddae8367c70101cebdc727cecbe879671e7568b4
fortec007 <olumightyfalomo@gmail.com>
2017-04-25 00:09:27 +0100

    Template literals didn't work. Back to concatenation

--- js/news.js
+++ js/news.js
@@ -4,35 +4,27 @@ $(document).ready(function()
 
 	function getNews()
 	{
-		let url = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=1cb3ca22268a477b9d2a2f65c940ecfa";
+		let url = "https://newsapi.org/v1/articles?source=mashable&sortBy=latest&apiKey=1cb3ca22268a477b9d2a2f65c940ecfa";
 
 		$.getJSON(url, function(data) 
 		{
 			console.log("JSON data has been retrieved");
-			let news = data.articles;
+
+			printNews(data);
 		});
 	}
 
-	function printNews(result)
-	{	
+	function printNews(data)
+	{
+		let result = data.articles;
 		let output = "";
 		for(let i in result)
 		{
-			let link = "<a href='" + result[i].url + "'> ";
-			let resultDiv = `<li class="well well-lg list-group-item">
-						<div class="row">
-							<div class="col-xs-offset-1">
-							</div>
-							<div class="col-xs-8">
-								<b><p><a href="${ link }">${ result[i].title }</a></p></b>
-								<p> ${ result[i].description }. </p>
-							</div>
-							<div class="col-xs-2">
-								<a href="${link}"><img src="${result[i].urlToImage}" height="150" width="190"></a>
-							</div>
-						</div>	
-					</li>`;
+			let link = "<a href='" + result[i].url + "' target='_blank'> ";
+			let resultDiv = "<li class='well well-lg list-group-item'><div class='row'><div class='col-xs-5'><b><p>" + link + result[i].title + "</a></p></b><p>"+ result[i].description + "</p></div><div class='col-xs-2'></div><div class='col-xs-3'>" + link + "<img src='" + result[i].urlToImage +"'  class='img-responsive img-rounded'></a></div></div></li>";
+			output += resultDiv;
 		}
+		$('#news-source').html("( " + data.source + " )")
 		$('#printResults').html(output);
 	}	
 
