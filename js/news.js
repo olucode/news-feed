c26c8c75d802b286965c30b026d5f9e5289604ae
fortec007 <olumightyfalomo@gmail.com>
2017-04-24 19:21:23 +0100

    Used template literals for html

--- js/news.js
+++ js/news.js
@@ -19,8 +19,19 @@ $(document).ready(function()
 		for(let i in result)
 		{
 			let link = "<a href='" + result[i].url + "'> ";
-			let resultDiv = "<li ='well well-lg list-group-item'><div class='row'><div class='col-xs-offset-1'></div><div class='col-xs-8'><b><p>" + link + result[i].title + "</a></p></b><p>"+ result[i].description + "</p></div><div class='col-xs-2'>" + link + "<img src='" + result[i].urlToImage +"' height='150' width='190'></a></div></div></li>";
-			output += resultDiv;
+			let resultDiv = `<li class="well well-lg list-group-item">
+						<div class="row">
+							<div class="col-xs-offset-1">
+							</div>
+							<div class="col-xs-8">
+								<b><p><a href="${ link }">${ result[i].title }</a></p></b>
+								<p> ${ result[i].description }. </p>
+							</div>
+							<div class="col-xs-2">
+								<a href="${link}"><img src="${result[i].urlToImage}" height="150" width="190"></a>
+							</div>
+						</div>	
+					</li>`;
 		}
 		$('#printResults').html(output);
 	}	
