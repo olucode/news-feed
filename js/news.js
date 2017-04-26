4b2f7b9a8c663283bed0e520d691a667911b4696
fortec007 <olumightyfalomo@gmail.com>
2017-04-26 05:51:49 +0100

    Removed all traces of wikiviewer. Added a links file

--- js/news.js
+++ js/news.js
@@ -1,48 +1,68 @@
-$(document).ready(function() 
-{
+
+$(document).ready(()=>{
 	getNews();
 
 	function getNews()
 	{
 		let endPoint = "https://newsapi.org/v1/articles";
 		let apiKey = "1cb3ca22268a477b9d2a2f65c940ecfa";
-		let urls = {
-			"bbc" : `${endPoint}?source=bbc-news&sortBy=top&apiKey=${apiKey} `,
-			"guardian-uk" : `${endPoint}?source=the-guardian-uk&sortBy=top&apiKey=${apiKey} `,
-			"hacker-news" : `${endPoint}?source=hacker-news&sortBy=top&apiKey=${apiKey} `,
-			"mashable" : `${endPoint}?source=mashable&sortBy=latest&apiKey=${apiKey} `,
-			"tnw": `${endPoint}?source=the-next-web&sortBy=latest&apiKey=${apiKey}`
-		};
+		let urls = [
+			`${endPoint}?source=google-news&sortBy=top&apiKey=${apiKey} `,
+			`${endPoint}?source=engadget&sortBy=latest&apiKey=${apiKey} `
+		];
 
-		let allResponses = {};
-		for(var i in urls)
-		{
-			$.getJSON(urls[i], function(data) 
-			{
-				console.log("JSON data has been retrieved");
-				let news = data.articles;
-				printNews(news);
-			});
-		}	
+		let allResults = [];
+
+		let count = urls.length-1;
+		const get =(real)=>{
+			$.getJSON(urls[ count ], function(data) {
+			console.log("JSON data has been retrieved from " + data.source);
+			let news = data.articles;
+			allResults.push(news)
+			// printNews(news);
+			real();
+			})
+		};
+		recurse();		
+
+		function recurse(){
+			if(count >= 0){
+				get(recurse);
+				count--;
+			}
+			else
+				printNews(allResults);
+		}
 	}
+
 
 	function printNews(result)
 	{	
+
+		let res=[];
+		result.map(list=>{
+			// console.log(list)
+			return list.map(item=>{
+				// console.log(item)
+				res.push(item)})})
+		console.log(res)
+		shuffleArray(res);		
 		let output = "";
-		for(let i in result)
+		for(let i = 0; i < res.length; i++)
 		{
-			let link = "<a href='" + result[i].url + "'> ";
+
+			let link =  res[i].url;
 			let resultDiv = `<li class="well well-lg list-group-item">
 						<div class="row">
 
 							<div class="col-xs-5">
-								<b><p><a href="${ link }" target="_blank">${ result[i].title }</a></p></b>
-								<p> ${ result[i].description }. </p>
+								<b><p><a href="${ link }" target="_blank">${ res[i].title }</a></p></b>
+								<p> ${ res[i].description }. </p>
 							</div>
 							<div class="col-xs-2">
 							</div>
 							<div class="col-xs-3">
-								<a href="${link}" target="_blank"><img src="${result[i].urlToImage}" class="img-responsive img-rounded"></a>
+								<a href="${link}" target="_blank"><img src="${res[i].urlToImage}" class="img-responsive img-rounded"></a>
 							</div>
 						</div>	
 					</li>`;
@@ -51,6 +71,14 @@ $(document).ready(function()
 		$('#printResults').html(output);
 	}	
 
+	function shuffleArray(array) {
+	    for (var i = array.length - 1; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	    }
+	}
 });
 
 //  LocalWords:  href
